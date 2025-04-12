7a8abaeff9c16b0f1ecd4b7d675f50fe8a5386a6
a2942 <77714719+a2942@users.noreply.github.com>
2025-04-13 01:39:20 +0800

    删除 backend/install.bat
    删除 backend/install.bat，因为文件存在**文件存在非法或不可见的污染字符**

--- backend/install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: 基础Python库安装
-pip install openai
-pip install transformers
-pip install torch
-pip install aiohttp
-pip install playsound
-pip install websockets==12.0
-pip install langdetect
