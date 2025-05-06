e0f55015ce7989c6c6df5f4cf67417b2620ba485
Vickko <vickkochyan@gmail.com>
2025-05-07 01:48:37 +0800

    feat(go-impl): 分离shadow和user表，优化user repo实现

---
+++ backend/go-impl/internal/data/ent/schema/shadow.go
@@ -0,0 +1,47 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Shadow holds the schema definition for the Shadow entity.
+// Shadow存储用户密码，与User表分离以提高安全性
+type Shadow struct {
+	ent.Schema
+}
+
+// Fields of the Shadow.
+func (Shadow) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int64("id").
+			Positive().
+			Immutable().
+			Unique(),
+		field.String("password").
+			NotEmpty().
+			Sensitive(),
+		// 用户ID，通过边关系可以自动填充
+		field.Int64("user_id").
+			Positive().
+			Unique(),
+	}
+}
+
+func (Shadow) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		TimestampMixin{},
+	}
+}
+
+// Edges of the Shadow.
+func (Shadow) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("user", User.Type).
+			Ref("shadow").
+			Field("user_id").
+			Unique().
+			Required(),
+	}
+}
