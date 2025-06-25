d2ff513d3d913944027a2115d45a2f9a97566a6c
Afiredream <afiredream@qq.com>
2025-06-25 14:48:05 +0800

    Update：优化前端request模块并且替换前端代码中的fetch函数为request模块对应的函数

--- frontend/public/js/settings/settings.js
+++ frontend/public/js/settings/settings.js
@@ -1,3 +1,5 @@
+import request from "../core/request.js";
+
 document.addEventListener("DOMContentLoaded", () => {
   const navMenu = document.getElementById("nav-menu");
   const settingsForm = document.getElementById("settings-form");
@@ -120,20 +122,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     showLoader(true);
     saveStatus.textContent = "";
-
-    try {
-      const response = await fetch("/api/settings/config", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
-
-      const result = await response.json();
-
-      if (!response.ok) {
-        throw new Error(result.detail || "保存失败");
-      }
-
+    return request.configSet(formData)
+    .then(async () => {
       saveStatus.textContent = result.message;
       saveStatus.style.color = "var(--success-color)";
 
@@ -144,24 +134,26 @@ document.addEventListener("DOMContentLoaded", () => {
       if (activeLink) {
         renderForm(activeLink.dataset.category, activeLink.dataset.subcategory);
       }
-    } catch (error) {
+      return
+    })
+    .catch(error => {
       saveStatus.textContent = `错误: ${error.message}`;
       saveStatus.style.color = "red";
-    } finally {
+      return
+    })
+    .then(() => {
       showLoader(false);
       setTimeout(() => {
         saveStatus.textContent = "";
       }, 5000);
-    }
+    })
   });
 
   // 初始加载配置
   async function loadConfig(selectFirst = true) {
     showLoader(true);
-    try {
-      const response = await fetch("/api/settings/config");
-      if (!response.ok) throw new Error("无法加载配置");
-      configData = await response.json();
+    return request.configGet()
+    .then(configData => {
       renderNavMenu();
 
       if (selectFirst) {
@@ -171,12 +163,16 @@ document.addEventListener("DOMContentLoaded", () => {
           firstSubLink.click();
         }
       }
-    } catch (error) {
+      return
+    })
+    .catch(error => {
       console.error(error);
       settingsForm.innerHTML = `<p style="color: red;">加载配置失败: ${error.message}</p>`;
-    } finally {
+      return
+    })
+    .then(() => {
       showLoader(false);
-    }
+    })
   }
 
   loadConfig();
