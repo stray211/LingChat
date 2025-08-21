b347bedb76897709456dc25dfa97604dc34436de
metaone01 <metaone01@outlook.com>
2025-08-21 13:09:33 +0800

    删除store中不必要的await

--- frontend_vue/src/api_neo/store.ts
+++ frontend_vue/src/api_neo/store.ts
@@ -2,8 +2,8 @@ import { UIStatus, createUIStatusStatic } from "./services/UIStatus.ts";
 import { UserInfo, UserData, createUserInfoStatic, createUserDataStatic } from "./services/UserInfo.ts";
 import { Settings, Defaults, createSettingsStatic, createDefaultsStatic } from "./services/Settings.ts";
 
-const uiStatus: UIStatus = await createUIStatusStatic();
-const userInfo: UserInfo = await createUserInfoStatic();
-const userData: UserData = await createUserDataStatic();
-const settings: Settings = await createSettingsStatic();
-const defaults: Defaults = await createDefaultsStatic();
+const uiStatus: UIStatus = createUIStatusStatic();
+const userInfo: UserInfo = createUserInfoStatic();
+const userData: UserData = createUserDataStatic();
+const settings: Settings = createSettingsStatic();
+const defaults: Defaults = createDefaultsStatic();
